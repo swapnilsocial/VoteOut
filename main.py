e3b217c6b3ed2b972f0da024423e648bd1327b4b
swapnilsocial <swapniljoshisj20@gmail.com>
2021-07-19 22:02:06 +0530

    new modifications

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import json
 
 def add_votes():
     with open('votes.json') as v:
-        v1=json.load(v)
-        cat_votes=v1.get('cats')
-        dog_votes=v1.get('dogs')
-        total_votes=sum(v1.values())
+        v1 = json.load(v)
+        # cat_votes = v1.get('cats')
+        # dog_votes = v1.get('dogs')
+        total_votes = sum(v1.values())
         print(cat_votes, dog_votes, total_votes)
 
 print(add_votes())
--- server.py
+++ server.py
@@ -1,28 +1,51 @@
-from flask import Flask, render_template, Response, Request, redirect, url_for
+from flask import Flask, render_template, Response, request, redirect, url_for
+import json
 
 app = Flask(__name__)
 
-def add_votes():
-    with open('votes.json') as v:
-        pass
 
-@app.route('/voteout')
+def current_votes():
+    with open('votes.json') as v:
+        v1 = json.load(v)
+        cat_votes = v1.get('cats')
+        dog_votes = v1.get('dogs')
+        total_votes = sum(v1.values())
+        return cat_votes, dog_votes, total_votes
+
+
+@app.route('/voteout', methods=['POST', 'GET'])
 def index():
     return render_template('index.html')
+
 
 @app.route('/voteout/pets')
 def vote_pets():
     return render_template('pets.html')
 
-@app.route('/voteout/cat/status' ,methods=['POST'])
-def cat_status():
-    voted = "You have voted for CAT. Vote registered"
-    return render_template('pets_voted.html', voted=voted)
 
-@app.route('/voteout/dog/status' ,methods=['POST'])
+@app.route('/voteout/cat/status', methods=['POST', 'GET'])
+def cat_status():
+    cat, dog, total = current_votes()
+    cat = cat + 1
+    total = total + 1
+    voted = "You have voted for CAT. Vote registered"
+    vote_stat = "CAT has {} out of {} votes".format(cat, total)
+    return render_template('pets_voted.html', voted=voted, vote_stat=vote_stat)
+
+@app.route('/voteout/dog/status', methods=['POST', 'GET'])
 def dog_status():
-    voted = "You have voted for Dog. Vote registered"
-    return render_template('pets_voted.html', voted=voted)
+    cat, dog, total = current_votes()
+    dog = dog + 1
+    total = total + 1
+    voted = "You have voted for DOG. Vote registered"
+    vote_stat = "DOG has {} out of {} votes".format(dog, total)
+    return render_template('pets_voted.html', voted=voted, vote_stat=vote_stat)
+
+@app.route('/create_polling', methods=['POST', 'GET'])
+def create_polling():
+    name=request.form.get('poll')
+    print(name)
+    return "work in progress...!!!"
 
 @app.route('/')
 def server_status():
