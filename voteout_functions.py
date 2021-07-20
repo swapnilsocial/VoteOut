5800189fb3a5f197d74fb1b04c54878fb1ce3f17
swapnilsocial <swapniljoshisj20@gmail.com>
2021-07-20 18:26:35 +0530

    abs path added

--- voteout_functions.py
+++ voteout_functions.py
@@ -1,14 +1,16 @@
 import json
-
+import os
+THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+vote_file = os.path.join(THIS_FOLDER, 'votes.json')
 
 def update_votes(cat_votes, dog_votes):
     D = {'cats': cat_votes, 'dogs': dog_votes}
-    with open('votes.json', 'w')as f:
+    with open(vote_file, 'w')as f:
         json.dump(D, f)
 
 
 def current_votes():
-    with open('votes.json') as v:
+    with open(vote_file) as v:
         v1 = json.load(v)
         cat_votes = v1.get('cats')
         dog_votes = v1.get('dogs')
